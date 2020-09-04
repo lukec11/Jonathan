b9b86903e084b422d461703d55317475c66879b3
Ben Aubin <ben@benaubin.com>
2020-09-04 13:55:57 -0500

    Extract shortcut callback into a function for reuse

--- index.js
+++ index.js
@@ -97,118 +97,116 @@ function localizeMessageTimes(originalMessage, timeMatches, timezoneOffset) {
   return convertedMessage;
 }
 
-//listen for shortcut
-app.shortcut(
-  'check_timestamps',
-  async ({ shortcut, ack, context, payload }) => {
-    let timeMatches;
+async function localizeMessageShortcut({ shortcut, ack, context, payload }) {
+  let timeMatches;
 
-    try {
-      // convert Slack's message timestamp to a Date object;
-      const messageTime = new Date(
-        Number(shortcut.message.ts.split('.')[0]) * 1000
-      );
+  try {
+    // convert Slack's message timestamp to a Date object;
+    const messageTime = new Date(
+      Number(shortcut.message.ts.split('.')[0]) * 1000
+    );
 
-      // escape the original message to prevent slack ping injection / double pings
-      const originalMessage = escapeMessage(shortcut.message.text);
+    // escape the original message to prevent slack ping injection / double pings
+    const originalMessage = escapeMessage(shortcut.message.text);
 
-      // get timezone matches from within the message
-      timeMatches = chrono.parse(originalMessage, messageTime);
+    // get timezone matches from within the message
+    timeMatches = chrono.parse(originalMessage, messageTime);
 
-      //check for potentially no matches
-      if (timeMatches.length === 0) {
-        return app.client.chat.postEphemeral({
-          token: process.env.SLACK_OAUTH_TOKEN,
-          text: `I couldn't find a time in the message to convert. If you think this is in error, please <https://github.com/lukec11/Jonathan/issues/new|file an issue>.`,
-          channel: shortcut.channel.id,
-          thread_ts: shortcut.message.ts,
-          user: shortcut.user.id
-        });
-      }
-
-      // Most of the time, a user will not provide a timezone in their message, so we'll hold it to simplify the base case.
-      const originalPoster = await app.client.users.info({
+    //check for potentially no matches
+    if (timeMatches.length === 0) {
+      return app.client.chat.postEphemeral({
         token: process.env.SLACK_OAUTH_TOKEN,
-        user: shortcut.message.user
+        text: `I couldn't find a time in the message to convert. If you think this is in error, please <https://github.com/lukec11/Jonathan/issues/new|file an issue>.`,
+        channel: shortcut.channel.id,
+        thread_ts: shortcut.message.ts,
+        user: shortcut.user.id
       });
+    }
 
-      // we devide by 60 to get the user's timezone offset in minutes, as expected by chrono
-      const timezoneOffset = originalPoster.user.tz_offset / 60;
+    // Most of the time, a user will not provide a timezone in their message, so we'll hold it to simplify the base case.
+    const originalPoster = await app.client.users.info({
+      token: process.env.SLACK_OAUTH_TOKEN,
+      user: shortcut.message.user
+    });
 
-      const convertedMessage = localizeMessageTimes(
-        originalMessage,
-        timeMatches,
-        timezoneOffset
-      ).replace(/^|\n/g, '\n>');
+    // we devide by 60 to get the user's timezone offset in minutes, as expected by chrono
+    const timezoneOffset = originalPoster.user.tz_offset / 60;
 
-      //check if shortcut runner is original messager
-      if (shortcut.message.user === shortcut.user.id) {
-        //show in thread with full visibility
-        await app.client.chat.postMessage({
-          token: process.env.SLACK_OAUTH_TOKEN,
-          channel: shortcut.channel.id,
-          thread_ts: shortcut.message.ts,
-          text:
-            `:sparkles: Here's <@${shortcut.message.user}>'s post in your timezone:\n` +
-            convertedMessage +
-            '\n\n<https://github.com/lukec11/Jonathan|How does this work?>.'
-        });
-      } else {
-        await app.client.views.open({
-          // The token you used to initialize your app is stored in the `context` object
-          token: context.botToken,
-          trigger_id: payload.trigger_id,
-          view: {
-            blocks: [
-              {
-                type: 'header',
-                text: {
-                  type: 'plain_text',
-                  text: "Here's that post in your timezone:",
-                  emoji: true
-                }
-              },
-              {
-                type: 'section',
-                text: {
-                  type: 'mrkdwn',
-                  text: convertedMessage
-                }
-              },
-              {
-                type: 'section',
-                text: {
-                  type: 'mrkdwn',
-                  text: `FYI, <@${shortcut.message.user}> is in ${originalPoster.user.tz_label}. You should tell them to trigger this shortcut on their own message so that I can magically convert the times for everyone.`
-                }
+    const convertedMessage = localizeMessageTimes(
+      originalMessage,
+      timeMatches,
+      timezoneOffset
+    ).replace(/^|\n/g, '\n>');
+
+    //check if shortcut runner is original messager
+    if (shortcut.message.user === shortcut.user.id) {
+      //show in thread with full visibility
+      await app.client.chat.postMessage({
+        token: process.env.SLACK_OAUTH_TOKEN,
+        channel: shortcut.channel.id,
+        thread_ts: shortcut.message.ts,
+        text:
+          `:sparkles: Here's <@${shortcut.message.user}>'s post in your timezone:\n` +
+          convertedMessage +
+          '\n\n<https://github.com/lukec11/Jonathan|How does this work?>.'
+      });
+    } else {
+      await app.client.views.open({
+        // The token you used to initialize your app is stored in the `context` object
+        token: context.botToken,
+        trigger_id: payload.trigger_id,
+        view: {
+          blocks: [
+            {
+              type: 'header',
+              text: {
+                type: 'plain_text',
+                text: "Here's that post in your timezone:",
+                emoji: true
               }
-            ]
-          }
-        });
-      }
-      //send response message
-    } catch (err) {
-      console.error(err);
-
-      Honeybadger.notify(err, {
-        context: {
-          timeMatches,
-          message: {
-            ts: shortcut.message.ts,
-            text: shortcut.message.text
-          },
-          channelId: shortcut.channel.id,
-          userId: shortcut.user.id,
-          teamId: shortcut.team.id
+            },
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: convertedMessage
+              }
+            },
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: `FYI, <@${shortcut.message.user}> is in ${originalPoster.user.tz_label}. You should tell them to trigger this shortcut on their own message so that I can magically convert the times for everyone.`
+              }
+            }
+          ]
         }
       });
-    } finally {
-      await ack(); // Acknowledge shortcut request
     }
+    //send response message
+  } catch (err) {
+    console.error(err);
+
+    Honeybadger.notify(err, {
+      context: {
+        timeMatches,
+        message: {
+          ts: shortcut.message.ts,
+          text: shortcut.message.text
+        },
+        channelId: shortcut.channel.id,
+        userId: shortcut.user.id,
+        teamId: shortcut.team.id
+      }
+    });
+  } finally {
+    await ack(); // Acknowledge shortcut request
   }
-);
+}
 
 (async () => {
+  app.shortcut('check_timestamps', localizeMessageShortcut);
+
   await app.start(3000);
   console.log('online');
 })();
